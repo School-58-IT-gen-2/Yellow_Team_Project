6b8623a71c824bd63adc8c529dee5b8ba83a4ab8
unknown <kusoknikita@yandex.ru>
2023-11-10 20:08:35 +0300

    not completed but started

--- second/warm-up_with_classes.py
+++ second/warm-up_with_classes.py
@@ -1,3 +1,4 @@
+# -- часть Никиты --
 class YellowList(list):
     def __init__(self, items):
         super().__init__(items)
@@ -11,3 +12,29 @@ class YellowList(list):
         print(new_items)
 
 yellow_list = YellowList((1, 2, 3))
+
+# -- часть Вовы --
+
+# Это заглушка класса dict
+class YellowDict(dict):
+    # подсказка - у класса dict есть метод __init__(self)
+    # подсказка - у класса dict есть метод __init__(self, sequence)
+    # подсказка - у класса dict есть метод __init__(self, **kwargs)
+    def some_method(self):
+        pass
+
+# -- Часть Димы --
+
+# Это заглушка класса set
+class YellowSet(set):
+    # подсказка - у класса set есть метод __init__(self, items)
+    def some_method(self):
+        pass
+
+
+# Это вызов конструктора списка с помощью кортежа.
+# То есть на вход при создании экземпляра передается кортеж.
+# Вывод в консоли должен получится [1, 2, 3], ведь мы ничего не меняли в заглушке класса... пока что=)
+# Это вызов конструктора множества с помощью списка.
+# То есть на вход при создании экземпляра передается список.
+yellow_set = YellowSet(['1', "q", "wow", '1'])
