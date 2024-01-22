2a16e53db14ec3ee7b61eafe82f0ecd3868f7460
jily64 <108062664+jily64@users.noreply.github.com>
2024-01-22 21:32:20 +0300

    Я сделал генерацию карты)))

--- Govnogame/View/render.py
+++ Govnogame/View/render.py
@@ -5,13 +5,21 @@ class Render:
         pass
 
     def render(self, data):
-        _image = Image.new(mode="RGBA", size=(1080, 1080), color=000000)
-        _image.show()
+        _map = Image.new(mode="RGBA", size=(1080, 1080), color=000000)
 
         _map_size = data["map_size"]
         _ground = ['ground1.png', 'ground2.png', 'ground3.png', 'ground4.png']
 
         _one_point_size = 1080//_map_size
-        
+        print(_one_point_size)
+
+        for i in range(_map_size):
+            for j in range(_map_size):
+                _cycle_ground = Image.open(f"res/{_ground[random.randint(0, 3)]}").resize((_one_point_size, _one_point_size))
+                _map.paste(_cycle_ground, (i*_one_point_size, j*_one_point_size))
+
+        _map.show()
+
+
 
 
