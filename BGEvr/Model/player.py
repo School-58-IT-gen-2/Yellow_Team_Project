0592e768ab69547ef1f49645dc4ab4041d2ec82e
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-02-04 18:02:39 +0300

    moves commit

--- BGEvr/Model/player.py
+++ BGEvr/Model/player.py
@@ -5,8 +5,20 @@ class Player:
         self.dataloader = Data(self.user_id)
         self.progress = self.dataloader.load_user_data()
 
-    def player_move(self):
-        pass
+    def player_move(self,move):
+        print(len(self.progress["map_data"][0]))
+        if self.progress["player_position"][1] > 0 and self.progress["player_position"][1] <= len(self.progress["map_data"][0]) and self.progress["player_position"][0] > 0 and self.progress["player_position"][0] <= len(self.progress["map_data"][0]):
+            if move == 'u':
+                self.progress["player_position"][1] -= 1 
+            if move == 'd':
+                self.progress["player_position"][1] += 1 
+            if move == 'r':
+                self.progress["player_position"][0] += 1 
+            if move == 'l':
+                self.progress["player_position"][0] -= 1 
+            self.dataloader.save_user_data()
+        else:
+            print("куда валишь??77?7??777777?77?7")
     
     def next_turn(self):
         pass
@@ -16,6 +28,4 @@ class Player:
 
     def player_info(self):
         return f"{self.progress["money"]} - деняк, \n{self.progress["units"]} - жителей"
-    
-
     
--- BGEvr/main.py
+++ BGEvr/main.py
@@ -1,8 +1,11 @@
-import Controller.gameplay_manager
+'''import Controller.gameplay_manager
 game = Controller.gameplay_manager.Game()
 game.create_house(name="small_factory", position=[4,7])
 game.create_house(name="small_factory", position=[3,4])
 game.create_house(name="small_factory", position=[2,3])
 game.create_house(name="small_house", position=[6, 4])
 
-game.show_map()
+game.show_map()'''
+from Model.player import *
+Dima = Player("-1")
+Dima.player_move("u")
