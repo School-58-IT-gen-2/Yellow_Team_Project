7b73f7448c9ef52555656942f37d5738947be9f1
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-03-04 22:09:54 +0300

    TASK COMMIT ALKSDJNFGAKLOSDGN

--- BGEvr/database_adapter.py
+++ BGEvr/database_adapter.py
@@ -17,18 +17,43 @@ class Adapter():
             self.conn = psycopg2.connect(
                 host=self.host,
                 port=self.port,
-                sslmode=self.sslmode,
+                #sslmode=self.sslmode,
                 dbname=self.dbname,
                 user=self.user,
                 password=self.password,
-                target_session_attrs=self.target_session_attrs
+                target_session_attrs=self.target_session_attrs,
+                #sslcert = open("cert.crt")
             )
             self.cursor = self.conn.cursor()
         except Exception as error:
             print(f'connection error:{error}')
 
     def select(self, table):
-        request = f'SELECT * FROM "Yellow_team_project"."{table}"'
+        request = f'SELECT * FROM "Galactic Empire"."{table}"'
+        self.cursor.execute(request)
+        data = self.cursor.fetchall()
+        return data
+    
+    def select_task_1(self):
+        request = f'SELECT * FROM "Galactic Empire"."Cruisers"'
+        self.cursor.execute(request)
+        data = self.cursor.fetchall()
+        return data
+
+    def select_task_2(self):
+        request = f"""SELECT * FROM "Galactic Empire"."Systems" as sys WHERE sys."Allegiance" = 'Empire' ORDER BY id ASC"""
+        self.cursor.execute(request)
+        data = self.cursor.fetchall()
+        return data
+    
+    def select_task_3(self):
+        request = """SELECT * FROM "Galactic Empire"."Planets" as pl WHERE pl."Name" != 'Mityas_planet' AND pl."Population" > 50 ORDER BY pl."Position" ASC;"""
+        self.cursor.execute(request)
+        data = self.cursor.fetchall()
+        return data
+    
+    def select_task_4(self):
+        request = """SELECT * FROM "Galactic Empire"."Planets" as pl WHERE pl."Name" != 'Mityas_planet' AND lower(pl."Name") = pl."Name" AND pl."Population" > 50 ORDER BY pl."Position" ASC;"""
         self.cursor.execute(request)
         data = self.cursor.fetchall()
         return data
@@ -46,7 +71,11 @@ class Adapter():
 
 
 
-db = Adapter(host="rc1d-9cjee2y71olglqhg.mdb.yandexcloud.net",port="6432",sslmode="verify-full",dbname="sch58_db",user="Admin",password="atdhfkm2024",target_session_attrs="read-write")
+db = Adapter(host="rc1d-9cjee2y71olglqhg.mdb.yandexcloud.net",port="6432",dbname="sch58_db",sslmode=None,user="Admin",password="atdhfkm2024",target_session_attrs="read-write")
 db.connect()
-houses_data = db.select("houses")
-print(houses_data)
+lvl1 = db.select_task_1()
+lvl2 = db.select_task_2()
+lvl3 = db.select_task_3()
+lvl4 = db.select_task_4()
+
+print(lvl1,"\n",lvl2,"\n",lvl3,'\n',lvl4)
