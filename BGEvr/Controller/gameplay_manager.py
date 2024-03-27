f58675bab114522bb0fac3b385ee49c53613960b
Lev Kerskiy-Gosling <levker08@icloud.com>
2024-03-27 19:27:47 +0300

    check commit

--- BGEvr/Controller/gameplay_manager.py
+++ BGEvr/Controller/gameplay_manager.py
@@ -1,14 +1,15 @@
 from Controller.Data_loader import Data
 from View import render
 from PIL import Image
+from database_adapter import *
 
 class Game():
     def __init__(self, user_id,db,player):
         self.player = player
         self.db = db
         self.user_id = user_id
-        self.data_loader = Data(self.user_id)
-        self.building_data = self.data_loader.load_game_data()
+        #self.data_loader = Data(self.user_id)
+        #self.building_data = self.data_loader.load_game_data()
         self.render = render.Render()
 
     def create_house(self, name, position):
@@ -20,12 +21,9 @@ class Game():
             _pos = [position[0]+i]
             _pos.append(position[1]+i)
             map_posing.append(_pos)
-        print(map_posing, 'Mathed MapPos')
         if self.check_can_build(map_posing):
-            print("Creating")
             _building["position"] = map_posing[0]
             self.build_price = _building["price"]
-            print(_building["price"])
             self.player.player_money -= self.build_price
 
             #self.user_data["map_data"] = _map
@@ -37,9 +35,7 @@ class Game():
             else:
                 t = str_of_houses.split(",")
                 t.append(str(_house_id[0]))
-                print(t)
                 str_of_houses = f"'{",".join(t)}'"
-            print(str_of_houses)
             building_request = f"""house_id = {str_of_houses}"""
             self.db.update("user_info",building_request,self.user_id)
             #print(_building["position"], "building complete pos")
@@ -52,12 +48,29 @@ class Game():
             print("Can't Create")
 
 
-    def check_can_build(self, maping):
-        return 1
+    def check_can_build(self):
+        _res = True
+        _houses_id = self.db.select_by_user_id("user_info",self.user_id)[0][3]
+        _houses_id = list(map(int,_houses_id.split(',')))
+        player_pos_x = self.db.select_by_user_id("user_info",self.user_id)[0][0]
+        player_pos_y = self.db.select_by_user_id("user_info",self.user_id)[0][1]
+        houses_data = self.db.select("houses")
+        for i in houses_data:
+            for j in _houses_id:
+                if j in i:
+                    if player_pos_x == i[1] and player_pos_y == i[2]:
+                        _res = False
+                        break
+
+        return _res
 
     def show_map(self):
         self.render.render(self.user_data).show()
 
     def save_pic(self):
         self.render.save_pic(self.user_id)
+
+
+
+
 
